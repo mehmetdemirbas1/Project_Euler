4e07fe35c7272fa09e79efc03bc664727917d169
Mehmet Demirbas <mehmet1demirbas@gmail.com>
2026-01-29 21:11:18 +0300

    Birinci soru

--- 1.c
+++ 1.c
@@ -2,6 +2,14 @@
 
 int main() {
    
-    
+    int sum = 0;
+    for (int i = 1; i < 1000; i++)
+    {
+        if(i%3==0 && i%5==0)
+        {
+            sum += i;
+        }
+    }
+    printf("Sum of multiples of both 3 and 5 below 1000 is: %d\n", sum);
     return 0;
 }
