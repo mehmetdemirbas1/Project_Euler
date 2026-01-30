4c29b70f48dfc2ee98121e50cc32aac2db52e594
Mehmet Demirbas <mehmet1demirbas@gmail.com>
2026-01-30 16:47:54 +0300

    3. soru eklendi

--- 3.c
+++ 3.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+int isprime(int val)
+{
+	if (val == 0 || val == 1)
+		return 0;
+
+	if (val % 2 == 0)
+		return val == 2;
+
+	if (val % 3 == 0)
+		return val == 3;
+
+	if (val % 5 == 0)
+		return val == 5;
+
+	else
+		for (int i = 7; i * i <= val; i += 2)
+			if (val % i == 0)
+				return 0;
+
+	return 1;
+}
+
 int main()
 {
-    
+
 }
